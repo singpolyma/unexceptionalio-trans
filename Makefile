695fab4cec988491e5d31de9dad3670907d84a1c
Stephen Paul Weber <singpolyma@singpolyma.net>
2018-05-07 21:17:23 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,39 @@
+GHCFLAGS=-Wall -fno-warn-tabs -fno-warn-orphans -fno-warn-name-shadowing -XHaskell98
+HLINTFLAGS=-XHaskell98 -XCPP -i 'Use camelCase' -i 'Use String' -i 'Use string literal' -i 'Use list comprehension' --utf8 -XMultiParamTypeClasses
+VERSION=0.1.0
+
+.PHONY: all clean doc install
+
+all: report.html doc dist/build/libHSunexceptionalio-trans-$(VERSION).a dist/unexceptionalio-trans-$(VERSION).tar.gz
+
+install: dist/build/libHSunexceptionalio-trans-$(VERSION).a
+	cabal install
+
+report.html: UnexceptionalIO/Trans.hs
+	-hlint $(HLINTFLAGS) --report UnexceptionalIO/Trans.hs
+
+doc: dist/doc/html/unexceptionalio-trans/index.html README
+
+README: unexceptionalio-trans.cabal
+	tail -n+$$(( `grep -n ^description: $^ | head -n1 | cut -d: -f1` + 1 )) $^ > .$@
+	head -n+$$(( `grep -n ^$$ .$@ | head -n1 | cut -d: -f1` - 1 )) .$@ > $@
+	-printf ',s/        //g\n,s/^.$$//g\nw\nq\n' | ed $@
+	$(RM) .$@
+
+dist/doc/html/unexceptionalio-trans/index.html: dist/setup-config UnexceptionalIO/Trans.hs 
+	cabal haddock --hyperlink-source
+
+dist/setup-config: unexceptionalio-trans.cabal
+	cabal configure
+
+clean:
+	find -name '*.o' -o -name '*.hi' | xargs $(RM)
+	$(RM) report.html
+	$(RM) -r dist dist-ghc
+
+dist/build/libHSunexceptionalio-trans-$(VERSION).a: unexceptionalio-trans.cabal dist/setup-config UnexceptionalIO/Trans.hs
+	cabal build --ghc-options="$(GHCFLAGS)"
+
+dist/unexceptionalio-trans-$(VERSION).tar.gz: unexceptionalio-trans.cabal dist/setup-config UnexceptionalIO/Trans.hs README
+	cabal check
+	cabal sdist
---
+++ UnexceptionalIO/Trans.hs
@@ -0,0 +1,127 @@
+{-# LANGUAGE CPP #-}
+-- | When you've caught all the exceptions that can be handled safely,
+--   this is what you're left with.
+--
+-- > runExceptIO . fromIO ≡ id
+--
+-- It is intended that you use qualified imports with this library.
+--
+-- > import UnexceptionalIO.Trans (UIO)
+-- > import qualified UnexceptionalIO.Trans as UIO
+module UnexceptionalIO.Trans (
+	UIO.UIO,
+	UIO.Unexceptional(..),
+	fromIO,
+	run,
+	runExceptIO,
+	-- * Unsafe entry points
+#ifdef __GLASGOW_HASKELL__
+	fromIO',
+#endif
+	UIO.unsafeFromIO,
+	-- * Pseudo exceptions
+	UIO.SomeNonPseudoException,
+#ifdef __GLASGOW_HASKELL__
+	UIO.PseudoException(..),
+	UIO.ProgrammerError(..),
+	UIO.ExternalError(..),
+	-- * Pseudo exception helpers
+	UIO.bracket,
+#if MIN_VERSION_base(4,6,0)
+	UIO.forkFinally,
+	UIO.fork
+#endif
+#endif
+) where
+
+import Control.Exception (Exception)
+import Control.Monad.IO.Class (MonadIO(..))
+#if MIN_VERSION_transformers(0,5,3)
+import qualified Control.Monad.Trans.Accum as Trans
+#endif
+import qualified Control.Monad.Trans.Class as Trans
+import qualified Control.Monad.Trans.Cont as Trans
+import qualified Control.Monad.Trans.Error as Trans
+import qualified Control.Monad.Trans.Except as Trans
+import qualified Control.Monad.Trans.Identity as Trans
+import qualified Control.Monad.Trans.List as Trans
+import qualified Control.Monad.Trans.Maybe as Trans
+import qualified Control.Monad.Trans.RWS.Lazy as RWSL
+import qualified Control.Monad.Trans.RWS.Strict as RWSS
+import qualified Control.Monad.Trans.Reader as Trans
+#if MIN_VERSION_transformers(0,5,3)
+import qualified Control.Monad.Trans.Select as Trans
+#endif
+import qualified Control.Monad.Trans.State.Lazy as StateL
+import qualified Control.Monad.Trans.State.Strict as StateS
+import qualified Control.Monad.Trans.Writer.Lazy as WriterL
+import qualified Control.Monad.Trans.Writer.Strict as WriterS
+import qualified UnexceptionalIO as UIO
+
+-- | Catch any exception but 'PseudoException' in an 'IO' action
+fromIO :: (UIO.Unexceptional m) => IO a -> Trans.ExceptT UIO.SomeNonPseudoException m a
+fromIO = Trans.ExceptT . UIO.fromIO
+
+-- | You promise that 'e' covers all exceptions but 'PseudoException'
+--   thrown by this 'IO' action
+--
+-- This function is partial if you lie
+fromIO' :: (Exception e, UIO.Unexceptional m) => IO a -> Trans.ExceptT e m a
+fromIO' = Trans.ExceptT . UIO.fromIO'
+
+-- | Re-embed 'UIO' into 'MonadIO'
+run :: (MonadIO m) => UIO.UIO a -> m a
+run = liftIO . UIO.run
+
+-- | Re-embed 'UIO' and possible exception back into 'IO'
+runExceptIO :: (Exception e, MonadIO m) => Trans.ExceptT e UIO.UIO a -> m a
+runExceptIO = liftIO . UIO.runEitherIO . Trans.runExceptT
+
+#if MIN_VERSION_transformers(0,5,3)
+instance (UIO.Unexceptional m) => UIO.Unexceptional (Trans.AccumT w m) where
+	lift = Trans.lift . UIO.lift
+#endif
+
+instance (UIO.Unexceptional m) => UIO.Unexceptional (Trans.ContT r m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m, Trans.Error e) => UIO.Unexceptional (Trans.ErrorT e m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m) => UIO.Unexceptional (Trans.ExceptT e m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m) => UIO.Unexceptional (Trans.IdentityT m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m) => UIO.Unexceptional (Trans.ListT m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m) => UIO.Unexceptional (Trans.MaybeT m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m, Monoid w) => UIO.Unexceptional (RWSL.RWST r w s m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m, Monoid w) => UIO.Unexceptional (RWSS.RWST r w s m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m) => UIO.Unexceptional (Trans.ReaderT r m) where
+	lift = Trans.lift . UIO.lift
+
+#if MIN_VERSION_transformers(0,5,3)
+instance (UIO.Unexceptional m) => UIO.Unexceptional (Trans.SelectT r m) where
+	lift = Trans.lift . UIO.lift
+#endif
+
+instance (UIO.Unexceptional m) => UIO.Unexceptional (StateL.StateT s m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m) => UIO.Unexceptional (StateS.StateT s m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m, Monoid w) => UIO.Unexceptional (WriterL.WriterT w m) where
+	lift = Trans.lift . UIO.lift
+
+instance (UIO.Unexceptional m, Monoid w) => UIO.Unexceptional (WriterS.WriterT w m) where
+	lift = Trans.lift . UIO.lift
